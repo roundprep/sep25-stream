291d67a46c23998927f26550c93de5a2f1ca276b
IM <indrani.mukherjee1808@gmail.com>
2025-12-20 11:19:33 -0500

    Initial Commit

--- Steam/src/com/acme/libmanager/model/Author.java
+++ Steam/src/com/acme/libmanager/model/Author.java
@@ -3,4 +3,20 @@ package com.acme.libmanager.model;
 public class Author {
     public String name;
     public String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
--- Steam/src/com/acme/libmanager/model/Book.java
+++ Steam/src/com/acme/libmanager/model/Book.java
@@ -8,4 +8,44 @@ public class Book {
     private String category;
     private double price;
     private List<String> tags;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
