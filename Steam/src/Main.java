9edfda874c64cea308dc29e7c52c242b2a2bbf53
Dharmendra Sengar <dsengar@Dharmendras-MacBook-Pro.local>
2025-12-20 11:51:02 -0500

    Updated

--- Steam/src/Main.java
+++ Steam/src/Main.java
@@ -1,5 +1,14 @@
+import com.acme.libmanager.service.LibManagerService;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-   
+    public static void main(String[] args) {
+        LibManagerService manager = new LibManagerService();
+
+        System.out.println(manager.getBookByTitle("Learning Java"));
+
+        System.out.println("manager.getBookTitles() = " + manager.getBookTitles());
+
+    }
 }
--- Steam/src/com/acme/libmanager/model/Author.java
+++ Steam/src/com/acme/libmanager/model/Author.java
@@ -3,4 +3,25 @@ package com.acme.libmanager.model;
 public class Author {
     public String name;
     public String email;
+
+    public Author(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
